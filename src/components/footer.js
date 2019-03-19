bd4aa9f3358537c2a65e2a5caf2d31df51cdfce1
Julian <36549405+julian-klinger@users.noreply.github.com>
2019-03-19 17:54:19 +0100

    typo fix

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,26 +7,25 @@ const Footer = ({ siteTitle }) => {
   return (
 
     <footer className="min-h-screen-third bg-green-light z-10 pt-6 pb-12">
-      <div class="flex flex-wrap justify-center">
-        <div class="w-1/3 py-1 px-2">
-          <div class="text-center">
-            <Link className="block sm:text-sm md:text-base font-light no-underline text-white hover:bg-white hover:text-green p-2" to="/imprint">Impressum</Link>
-            <Link className="block sm:text-sm md:text-base font-light no-underline text-white hover:bg-white hover:text-green p-2" to="/imprint">Über uns</Link>
-            <Link className="block sm:text-sm md:text-base font-light no-underline text-white hover:bg-white hover:text-green p-2" to="/imprint">FAQ </Link>
+      <div className="flex flex-wrap justify-center">
+        <div className="w-1/3 py-1 px-2">
+          <div className="text-center">
+            <Link className="block sm:text-sm md:text-base font-light no-underline hover:bg-white hover:text-green p-2" to="/datenschutz">Datenschutz</Link>
+            <Link className="block sm:text-sm md:text-base font-light no-underline hover:bg-white hover:text-green p-2" to="/about">Über uns</Link>
+            <Link className="block sm:text-sm md:text-base font-light no-underline hover:bg-white hover:text-green p-2" to="/faq">FAQ </Link>
           </div>
         </div>
-        <div class="w-1/3 p-2">
-          <div class="text-center">
+        <div className="w-1/3 p-2">
+          <div className="text-center">
             <img src={logo} className="w-5 md:w-16" />
-            <h2 className="text-white text-lg">Klimafuchs</h2>
-            <p className="text-white text-sm text-thin">Clever CO2 sparen!</p>
+            <h2 className="text-lg">Klimafuchs</h2>
+            <p className="text-sm text-thin">Clever CO2 sparen!</p>
           </div>
         </div>
-        <div class="w-1/3 py-1 px-2">
-          <div class="text-center">
-            <Link className="block sm:text-sm md:text-base font-light no-underline text-white hover:bg-white hover:text-green p-2" to="/imprint">Datenschutz</Link>
-            <Link className="block sm:text-sm md:text-base font-light no-underline text-white hover:bg-white hover:text-green p-2" to="/imprint">Mach mit!</Link>
-            <Link className="block sm:text-sm md:text-base font-light no-underline text-white hover:bg-white hover:text-green p-2" to="/imprint">Unterstützen</Link>
+        <div className="w-1/3 py-1 px-2">
+          <div className="text-center">
+            <Link className="block sm:text-sm md:text-base font-light no-underline hover:bg-white hover:text-green p-2" to="/imprint">Impressum</Link>
+            <Link className="block sm:text-sm md:text-base font-light no-underline hover:bg-white hover:text-green p-2" to="/wiki-link">Mach mit!</Link>
           </div>
         </div>
 
